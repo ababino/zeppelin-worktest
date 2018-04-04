210b223d61d13de077128eee355a8b25ebb32f36
Andres Babino <ababino@gmail.com>
2018-04-03 21:38:40 -0300

    initial commit

---
+++ contracts/DAICO.sol
@@ -0,0 +1,165 @@
+pragma solidity ^0.4.19;
+
+import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
+import 'zeppelin-solidity/contracts/ownership/Ownable.sol';
+import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import 'zeppelin-solidity/contracts/math/Math.sol';
+import 'zeppelin-solidity/contracts/math/SafeMath.sol';
+
+
+contract DAICO is Crowdsale, Ownable {
+    /*using Roles for Roles.Role;*/
+
+    ERC20 public token;
+    uint256 public tap;
+    uint256 public lastWithdrawn;
+
+    /*iquorum is the ivnerse quorum. 3 is a 1/3, and so on.*/
+    uint256 public iquorum;
+    uint256 public quorum;
+    uint256 public numberOfHolders;
+    RaiseTapProposal[] public proposals;
+
+    /**
+    * @dev The owner of the contract should implement how the memebers are
+    * added. Probably, some background check to verify its identity will be
+    * needed. Also, a delay could be needed to avoid double voting.
+    */
+    mapping (address => bool) public isHolder;
+
+    event RaiseTapProposalAdded(uint256 proposalID, address author, uint256 amount);
+    event Voted(uint256 proposalID, address voter, bool supports);
+
+    struct RaiseTapProposal {
+        address author;
+        uint256 proposedNewTap;
+        uint256 votingDeadline;
+        bool executed;
+        uint256 numberOfVotes;
+        uint256 numberOfPositiveVotes;
+        mapping (address => bool) voted;
+    }
+
+    function DAICO
+        (
+          uint256 _lastWithdrawn,
+          uint256 _rate,
+          uint256 _tap,
+          uint256 _iquorum,
+          address _wallet,
+          ERC20 _token
+        )
+        public
+        Crowdsale(_rate, _wallet, _token)
+        {
+            tap = _tap;
+            lastWithdrawn = _lastWithdrawn;
+            iquorum = _iquorum;
+            quorum = 0;
+        }
+
+        /**
+        * @dev Add buyers to the holder list. If you want to allow holders to
+        * change after the selling period you should implement addHolder and
+        * removeHolder functions
+        */
+        function _updatePurchasingState(address _beneficiary, uint256 _weiAmount) internal {
+          require(_beneficiary != address(0));
+          require(_weiAmount != 0);
+          isHolder[_beneficiary] = true;
+          numberOfHolders++;
+          if (numberOfHolders % iquorum == 0){
+            quorum++;
+          }
+        }
+
+        /**
+        * @dev The forwardFunds function should not send any thing to the
+        * owner. It should keep the eth to be delivered as the holders decide.
+        */
+        function _forwardFunds() internal {
+        }
+
+        /**
+        * @dev Only the owner can withdraw funds, and only as much as it is
+        * allowed by the tap parameter.
+        */
+        function widthraw() public onlyOwner {
+            require(block.timestamp > lastWithdrawn);
+            uint256 allowed  = (block.timestamp - lastWithdrawn) * tap;
+            uint256 amount = Math.min256(allowed, address(this).balance);
+            msg.sender.transfer(amount);
+            lastWithdrawn = block.timestamp;
+          }
+
+        /**
+        * @dev Any holder can propose to increase the tap.
+        * @param proposedNewTap The new value of the tap variable. It must be grater than the current one.
+        * @param timeToDabate The time since current block, in seconds, to vote.
+        */
+        function newRaiseTapProposal(
+          uint256 proposedNewTap,
+          uint256 timeToDabate
+        )
+          public
+          returns (uint256 proposalID)
+        {
+          require(proposedNewTap > tap);
+          require(isHolder[msg.sender]);
+
+          proposalID = proposals.length++;
+          RaiseTapProposal storage p = proposals[proposalID];
+          p.author = msg.sender;
+          p.proposedNewTap = proposedNewTap;
+          p.votingDeadline = block.timestamp + timeToDabate;
+          p.executed = false;
+          p.numberOfVotes = 0;
+          p.numberOfPositiveVotes = 0;
+          RaiseTapProposalAdded(proposalID, msg.sender, proposedNewTap);
+          return proposalID;
+        }
+
+        /**
+        * @dev Any holder can vote.
+        * @param proposalID The ID of the proposal to vote for or against of.
+        * @param supportsProposal if true the holder supports the proposal, if false she does not.
+        */
+        function vote(
+          uint256 proposalID,
+          bool supportsProposal
+        )
+          public
+          returns (uint256 voteID)
+        {
+          require(isHolder[msg.sender]);
+
+          RaiseTapProposal storage p = proposals[proposalID];
+          require(!p.voted[msg.sender]);
+          require(p.votingDeadline < block.timestamp);
+
+          p.voted[msg.sender] = true;
+          p.numberOfVotes++;
+          if (supportsProposal) {
+            p.numberOfPositiveVotes++;
+          }
+          Voted(proposalID, msg.sender, supportsProposal);
+          return p.numberOfVotes;
+        }
+
+      /**
+      * @dev Any one can excecute a propasal. If the voting period has ended,
+      * there are more votes in favor than aginst the proposal, and the number
+      * of votes is greater than the quorum, then, the tap variable is updated
+      * to the propsed new tap.
+      * @param proposalID The ID of the proposal to execute.
+      */
+      function executeRaiseTapProposal(uint256 proposalID) private {
+        RaiseTapProposal storage p = proposals[proposalID];
+        require(p.votingDeadline > block.timestamp);
+        require(!p.executed);
+        p.executed = true;
+        if (SafeMath.mul(2, p.numberOfPositiveVotes) > SafeMath.add(p.numberOfVotes, quorum)){
+          tap = p.proposedNewTap;
+        }
+      }
+}
---
+++ test/DAICO.test.js
@@ -0,0 +1,101 @@
+import ether from 'zeppelin-solidity/test/helpers/ether';
+import { advanceBlock } from 'zeppelin-solidity/test/helpers/advanceToBlock';
+import { increaseTimeTo, duration } from 'zeppelin-solidity/test/helpers/increaseTime';
+import latestTime from 'zeppelin-solidity/test/helpers/latestTime';
+import EVMRevert from 'zeppelin-solidity/test/helpers/EVMRevert';
+// import SimpleToken from 'zeppelin-solidity/contracts/examples/SimpleToken';
+const assertRevert = require('zeppelin-solidity/test/helpers/assertRevert')
+
+const BigNumber = web3.BigNumber;
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+const DAICO = artifacts.require('DAICO');
+// const SimpleToken = artifacts.require('../node_modules/zeppelin-solidity/contracts/examples/SimpleToken');
+const SimpleToken = artifacts.require('SimpleToken');
+
+contract('DAICO', function (accounts) {
+  const rate = new BigNumber(1);
+  const tap = 10000000000;
+  const iquorum = 3;
+  const wallet = accounts[1];
+  const value = ether(1);
+  const higher_value = ether(3);
+  const tokenSupply = new BigNumber('1e22');
+
+  before(async function () {
+    // Advance to the next block to correctly read time in the solidity "now" function interpreted by testrpc
+    await advanceBlock();
+  });
+
+  beforeEach(async function () {
+    this.lastWithdrawn = latestTime() + duration.weeks(1);
+    this.afterlastWithdrawn = this.lastWithdrawn + duration.weeks(1)
+    this.token = await SimpleToken.new();
+    this.daico = await DAICO.new(this.lastWithdrawn, rate, tap, iquorum, wallet, this.token.address);
+    await this.token.transfer(this.daico.address, tokenSupply);
+  });
+
+
+  describe('withdraw', function () {
+
+
+    it('owner can widthraw after last withdrawal', async function () {
+      let owner = accounts[0];
+      let no_owner = accounts[3];
+      await this.daico.buyTokens(no_owner, {from: no_owner, value: higher_value}).should.be.fulfilled;
+      await increaseTimeTo(this.afterlastWithdrawn);
+      await this.daico.widthraw({from: owner}).should.be.fulfilled;
+    });
+
+    it('no owner can not widthraw after end', async function () {
+      let no_owner = accounts[3];
+      await this.daico.buyTokens(no_owner, {from: no_owner, value: higher_value}).should.be.fulfilled;
+      await increaseTimeTo(this.afterlastWithdrawn);
+      await this.daico.widthraw({from: no_owner}).should.be.rejectedWith(EVMRevert);
+    });
+
+    it('owner can withdraw', async function () {
+      let owner = accounts[0];
+      let no_owner = accounts[3];
+      await this.daico.buyTokens(no_owner, {from: no_owner, value: higher_value}).should.be.fulfilled;
+      await increaseTimeTo(this.afterlastWithdrawn);
+      const initial_balance = web3.eth.getBalance(owner).c[0];
+      await this.daico.widthraw({from: owner}).should.be.fulfilled;
+      const final_balance = web3.eth.getBalance(owner).c[0];
+      assert.ok(final_balance > initial_balance);
+    });
+
+    it('owner can t withdraw more than tap times time', async function () {
+      let owner = accounts[0];
+      let no_owner = accounts[3];
+      await this.daico.buyTokens(no_owner, {from: no_owner, value: higher_value}).should.be.fulfilled;
+      await increaseTimeTo(this.afterlastWithdrawn);
+      const initial_balance = web3.eth.getBalance(owner).c[0];
+      const timestamp = web3.eth.getBlock(web3.eth.blockNumber).timestamp;
+      await this.daico.widthraw({from: owner});
+      const final_balance = web3.eth.getBalance(owner).c[0];
+      assert.ok(final_balance - initial_balance <= tap * (timestamp - this.lastWithdrawn))
+    });
+  });
+
+  describe('voting as expected', function () {
+    it('no purchaser no holder', async function() {
+      let purchaser = accounts[5];
+      let isholder = await this.daico.isHolder(purchaser);
+      assert.ok(!isholder);
+    });
+
+    it('purchaser is holder', async function() {
+      let purchaser = accounts[4];
+      await this.daico.buyTokens(purchaser, {from: purchaser, value: higher_value}).should.be.fulfilled;
+      let isholder = await this.daico.isHolder(purchaser);
+      assert.ok(isholder);
+    });
+
+  });
+
+});
