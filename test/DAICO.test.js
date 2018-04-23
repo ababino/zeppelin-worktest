fd9aac6e8144306ce2dc8db654336d18d7618960
Andres Babino <ababino@gmail.com>
2018-04-22 23:45:08 -0300

    adapt to new daico plus daicogovern contract structure

--- test/DAICO.test.js
+++ test/DAICO.test.js
@@ -13,6 +13,7 @@ require('chai')
   .should();
 
 const DAICO = artifacts.require('DAICO');
+const DaicoGovern = artifacts.require('DaicoGovern');
 const SimpleToken = artifacts.require('SimpleToken');
 
 contract('DAICO', function (accounts) {
@@ -38,7 +39,9 @@ contract('DAICO', function (accounts) {
     this.no_purchaser = accounts[3];
     this.owner_initial_balance = web3.eth.getBalance(this.owner);
     this.token = await SimpleToken.new();
-    this.daico = await DAICO.new(this.lastWithdrawn, rate, iquorum, wallet, this.token.address);
+    // this.daicoGovern = await DaicoGovern.new(this.lastWithdrawn, iquorum);
+    this.daico = await DAICO.new(rate, wallet, this.token.address, this.lastWithdrawn, iquorum);
+    this.daicoGovern = DaicoGovern.at(await this.daico.daicoGovernAddress())
     await this.token.transfer(this.daico.address, tokenSupply);
 
   });
@@ -48,12 +51,15 @@ contract('DAICO', function (accounts) {
 
     it('tokens can be bought and buyers are holders', async function () {
       await this.daico.buyTokens(this.purchaser, {from: this.purchaser, value: higher_value}).should.be.fulfilled;
-      const isholder = await this.daico.isHolder(this.purchaser);
+      const isholder = await this.daicoGovern.isHolder(this.purchaser);
       assert.ok(isholder);
+      // await this.daicoGovern.addHolder(this.no_purchaser, {from: this.no_purchaser}).should.be.rejectedWith(EVMRevert);
+      // await this.daicoGovern.addHolder(this.no_purchaser, {from: this.owner}).should.be.rejectedWith(EVMRevert);
+
     })
 
     it('if you dont buy you are not a holder', async function() {
-      const isholder = await this.daico.isHolder(this.no_purchaser);
+      const isholder = await this.daicoGovern.isHolder(this.no_purchaser);
       assert.ok(!isholder);
     });
 
@@ -66,13 +72,13 @@ contract('DAICO', function (accounts) {
 
     it('no one can propose a new tap', async function() {
       await this.daico.buyTokens(this.purchaser, {from: this.purchaser, value: higher_value});
-      await this.daico.newRaiseTapProposal(tap, 60, {from: this.purchaser}).should.be.rejectedWith(EVMRevert);
-      await this.daico.newRaiseTapProposal(tap, 60, {from: this.no_purchaser}).should.be.rejectedWith(EVMRevert);
-      await this.daico.newRaiseTapProposal(tap, 60, {from: this.owner}).should.be.rejectedWith(EVMRevert);
+      await this.daicoGovern.newRaiseTapProposal(tap, 60, {from: this.purchaser}).should.be.rejectedWith(EVMRevert);
+      await this.daicoGovern.newRaiseTapProposal(tap, 60, {from: this.no_purchaser}).should.be.rejectedWith(EVMRevert);
+      await this.daicoGovern.newRaiseTapProposal(tap, 60, {from: this.owner}).should.be.rejectedWith(EVMRevert);
     });
 
     it('initial value of tap is zero', async function(){
-      const currentTap = await this.daico.tap();
+      const currentTap = await this.daicoGovern.tap();
       expect(currentTap).to.be.zero;
     });
 
@@ -95,40 +101,40 @@ contract('DAICO', function (accounts) {
     });
 
     it('if you did not buy tokens you are not a holder', async function() {
-      const isholder = await this.daico.isHolder(this.no_purchaser);
+      const isholder = await this.daicoGovern.isHolder(this.no_purchaser);
       assert.ok(!isholder);
     });
 
     it('purchasers are considered holders', async function() {
-      const isholder = await this.daico.isHolder(this.purchaser);
+      const isholder = await this.daicoGovern.isHolder(this.purchaser);
       assert.ok(isholder);
     });
 
     it('holder can propose a new tap', async function() {
-      await this.daico.newRaiseTapProposal(tap, 3600, {from: this.purchaser}).should.be.fulfilled;
+      await this.daicoGovern.newRaiseTapProposal(tap, 3600, {from: this.purchaser}).should.be.fulfilled;
     });
 
     it('if you are not a holder you cant propose a new tap', async function() {
-      await this.daico.newRaiseTapProposal(tap, 3600, {from: this.no_purchaser}).should.be.rejectedWith(EVMRevert);
+      await this.daicoGovern.newRaiseTapProposal(tap, 3600, {from: this.no_purchaser}).should.be.rejectedWith(EVMRevert);
     });
 
 
     describe('voting', function(){
 
       beforeEach(async function(){
-        this.proposalID = await this.daico.newRaiseTapProposal.call(100, 3600, {from: this.purchaser});
-        await this.daico.newRaiseTapProposal(100, 3600, {from: this.purchaser});
+        this.proposalID = await this.daicoGovern.newRaiseTapProposal.call(100, 3600, {from: this.purchaser});
+        await this.daicoGovern.newRaiseTapProposal(100, 3600, {from: this.purchaser});
       });
 
       it('holder can vote a proposal before votingDeadline', async function() {
-        let proposal = await this.daico.proposals.call(this.proposalID)
+        let proposal = await this.daicoGovern.proposals.call(this.proposalID)
         const initalNumberOfVotes = proposal[4];
         const initalNumberOfPositiveVotes = proposal[5];
         expect(initalNumberOfVotes).to.be.zero;
         expect(initalNumberOfPositiveVotes).to.be.zero;
-        await this.daico.vote(this.proposalID, true, {from: this.purchaser}).should.be.fulfilled;
-        await this.daico.vote(this.proposalID, true, {from: this.no_purchaser}).should.be.rejectedWith(EVMRevert);
-        proposal = await this.daico.proposals.call(this.proposalID)
+        await this.daicoGovern.vote(this.proposalID, true, {from: this.purchaser}).should.be.fulfilled;
+        await this.daicoGovern.vote(this.proposalID, true, {from: this.no_purchaser}).should.be.rejectedWith(EVMRevert);
+        proposal = await this.daicoGovern.proposals.call(this.proposalID)
         const finalNumberOfVotes = proposal[4];
         const finalNumberOfPositiveVotes = proposal[5];
         finalNumberOfVotes.should.be.bignumber.equal(1);
@@ -137,40 +143,42 @@ contract('DAICO', function (accounts) {
 
       it('no one can vote a proposal after votingDeadline', async function() {
         await increaseTimeTo(latestTime() + duration.hours(1));
-        await this.daico.vote(this.proposalID, true, {from: this.purchaser}).should.be.rejectedWith(EVMRevert);
+        await this.daicoGovern.vote(this.proposalID, true, {from: this.purchaser}).should.be.rejectedWith(EVMRevert);
       });
 
       describe('proposal execution', function(){
 
         beforeEach(async function(){
-          this.initial_tap = await this.daico.tap();
-          this.rejectedProposalID = await this.daico.newRaiseTapProposal.call(100, 3600, {from: this.purchaser});
-          await this.daico.newRaiseTapProposal(200, 1800, {from: this.purchaser});
-          this.proposalWithoutQuorumID = await this.daico.newRaiseTapProposal.call(100, 3600, {from: this.purchaser});
-          await this.daico.newRaiseTapProposal(200, 1800, {from: this.purchaser});
-          this.oldProposalWithLowTapID = await this.daico.newRaiseTapProposal.call(90, 7200, {from: this.purchaser});
-          await this.daico.newRaiseTapProposal(90, 7200, {from: this.purchaser});
-          await this.daico.vote(this.proposalID, true, {from: this.purchaser});
-          await this.daico.vote(this.oldProposalWithLowTapID, true, {from: this.purchaser});
-          await this.daico.vote(this.rejectedProposalID, false, {from: this.purchaser});
+          this.initial_tap = await this.daicoGovern.tap();
+          this.rejectedProposalID = await this.daicoGovern.newRaiseTapProposal.call(100, 3600, {from: this.purchaser});
+          await this.daicoGovern.newRaiseTapProposal(200, 1800, {from: this.purchaser});
+          this.proposalWithoutQuorumID = await this.daicoGovern.newRaiseTapProposal.call(100, 3600, {from: this.purchaser});
+          await this.daicoGovern.newRaiseTapProposal(200, 1800, {from: this.purchaser});
+          this.oldProposalWithLowTapID = await this.daicoGovern.newRaiseTapProposal.call(90, 7200, {from: this.purchaser});
+          await this.daicoGovern.newRaiseTapProposal(90, 7200, {from: this.purchaser});
+          await this.daicoGovern.vote(this.proposalID, true, {from: this.purchaser});
+          await this.daicoGovern.vote(this.oldProposalWithLowTapID, true, {from: this.purchaser});
+          await this.daicoGovern.vote(this.rejectedProposalID, false, {from: this.purchaser});
         });
 
         it('proposal cant be executed before deadline', async function() {
-            await this.daico.executeRaiseTapProposal(this.proposalID).should.be.rejectedWith(EVMRevert);
+            await this.daicoGovern.executeRaiseTapProposal(this.proposalID).should.be.rejectedWith(EVMRevert);
         });
 
         it('passed propasal can be executed', async function() {
           await increaseTimeTo(latestTime() + duration.hours(1) + duration.seconds(1));
-          let proposal = await this.daico.proposals.call(this.proposalID);
-          let executed = proposal[3]
+          let proposals = await this.daicoGovern.proposals.call(this.proposalID);
+          let executed = proposals[3]
           assert.ok(!executed)
-          await this.daico.executeRaiseTapProposal(this.proposalID);
-          const newTap = await this.daico.tap();
+          const initialAmountAvailableToWithdraw = await this.daicoGovern.amountAvailableToWithdraw();
+          await this.daicoGovern.executeRaiseTapProposal(this.proposalID);
+          const finalAmountAvailableToWithdraw = await this.daicoGovern.amountAvailableToWithdraw();
+          const newTap = await this.daicoGovern.tap();
           newTap.should.be.bignumber.equal(100)
-          proposal = await this.daico.proposals.call(this.proposalID);
-          executed = proposal[3]
+          proposals = await this.daicoGovern.proposals.call(this.proposalID);
+          executed = proposals[3]
           assert.ok(executed)
-          const lastWithdrawn = await this.daico.lastWithdrawn();
+          const lastWithdrawn = await this.daicoGovern.lastWithdrawn();
           lastWithdrawn.should.be.bignumber.equal(latestTime())
         });
 
@@ -178,16 +186,19 @@ contract('DAICO', function (accounts) {
 
           beforeEach(async function(){
             await increaseTimeTo(latestTime() + duration.hours(1) + duration.seconds(1));
-            await this.daico.executeRaiseTapProposal(this.proposalID);
-            this.newTap = await this.daico.tap();
+            await this.daicoGovern.executeRaiseTapProposal(this.proposalID);
+            this.newTap = await this.daicoGovern.tap();
           });
 
           it('owner can withdraw time times new tap', async function(){
-            const firstWithdraw = await this.daico.lastWithdrawn();
+
+            const firstWithdraw = await this.daicoGovern.lastWithdrawn();
+
             await increaseTimeTo(latestTime() + duration.seconds(1000000));
+
             const initialBalance = web3.eth.getBalance(this.owner);
             const out = await this.daico.withdraw({from: this.owner});
-            const secondWithdraw = await this.daico.lastWithdrawn();
+            const secondWithdraw = await this.daicoGovern.lastWithdrawn();
             secondWithdraw.should.be.bignumber.equal(latestTime())
             const expectedWithdraw = this.newTap.mul(secondWithdraw.minus(firstWithdraw))
             const gasUsed = new BigNumber(out.receipt.gasUsed);
@@ -201,39 +212,39 @@ contract('DAICO', function (accounts) {
 
 
           it('propossal cant be executed more than one time', async function(){
-            await this.daico.executeRaiseTapProposal(this.proposalID).should.be.rejectedWith(EVMRevert);
+            await this.daicoGovern.executeRaiseTapProposal(this.proposalID).should.be.rejectedWith(EVMRevert);
           });
 
           it('cant propse lower the tap', async function(){
             const lowTap = this.newTap.minus(new BigNumber(1));
-            await this.daico.newRaiseTapProposal(lowTap, 3600, {from: this.purchaser}).should.be.rejectedWith(EVMRevert);
+            await this.daicoGovern.newRaiseTapProposal(lowTap, 3600, {from: this.purchaser}).should.be.rejectedWith(EVMRevert);
           });
 
           it('old proposal with low tap cant be executed', async function(){
             await increaseTimeTo(latestTime() + duration.hours(2) + duration.seconds(1));
-            await this.daico.executeRaiseTapProposal(this.oldProposalWithLowTapID).should.be.rejectedWith(EVMRevert);
-            const currentTap = await this.daico.tap();
+            await this.daicoGovern.executeRaiseTapProposal(this.oldProposalWithLowTapID).should.be.rejectedWith(EVMRevert);
+            const currentTap = await this.daicoGovern.tap();
             currentTap.should.be.bignumber.equal(this.newTap)
           });
 
           it('can propose a greater tap', async function(){
-            const currentTap = await this.daico.tap();
+            const currentTap = await this.daicoGovern.tap();
             const greaterTap = this.newTap.add(new BigNumber(1));
-            await this.daico.newRaiseTapProposal(greaterTap, 3600, {from: this.purchaser}).should.be.fulfilled;
+            await this.daicoGovern.newRaiseTapProposal(greaterTap, 3600, {from: this.purchaser}).should.be.fulfilled;
           });
 
         });
         it('rejected propasal can not be executed', async function() {
           await increaseTimeTo(latestTime() + duration.hours(1) + duration.seconds(1));
-          await this.daico.executeRaiseTapProposal(this.rejectedProposalID);
-          const newTap = await this.daico.tap();
+          await this.daicoGovern.executeRaiseTapProposal(this.rejectedProposalID);
+          const newTap = await this.daicoGovern.tap();
           newTap.should.be.zero
         });
 
         it('propasal without quorum can not be executed', async function() {
           await increaseTimeTo(latestTime() + duration.hours(1) + duration.seconds(1));
-          await this.daico.executeRaiseTapProposal(this.proposalWithoutQuorumID);
-          const newTap = await this.daico.tap();
+          await this.daicoGovern.executeRaiseTapProposal(this.proposalWithoutQuorumID);
+          const newTap = await this.daicoGovern.tap();
           newTap.should.be.zero
         });
 
